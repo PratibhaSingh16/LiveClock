6988bfd07b2a604e6397c40b224f952890e93737
PratibhaSingh16 <77543839+PratibhaSingh16@users.noreply.github.com>
2021-05-05 11:57:57 -0400

    code

---
+++ clock.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+label = Label(root, font=("ds-digital", 80), background = "white", foreground = "black")
+label.pack(anchor='center')
+time()
+
+mainloop()
+
+
+# In[ ]:
+
+
+
+
